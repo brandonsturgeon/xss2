bf09b4bc0c46ac471a1d42b35d5971314d31c37a
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-06 13:42:42 -0500

    Update this.js

--- this.js
+++ this.js
@@ -4,5 +4,26 @@ var grill = "";
 while(1) {
   grill = grill + "meat";
   console.log(grill);
-  console.log("It's getting hot!");
+ console.log("It's getting hot!");
 }
+
+//var HttpClient = function() {
+//    this.get = function(aUrl, aCallback) {
+//        var anHttpRequest = new XMLHttpRequest();
+//        anHttpRequest.onreadystatechange = function() { 
+//            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+//                aCallback(anHttpRequest.responseText);
+//        }
+//
+//        anHttpRequest.open( "GET", aUrl, true );            
+//        anHttpRequest.send( null );
+//    }
+//}
+
+//aClient = new HttpClient();
+
+//while(1) {
+//  aClient.get('https://scholar.princeton.edu/sites/default/files/oversize_pdf_test_0.pdf', function(response) {
+//    console.log(response);
+//  });
+//};
