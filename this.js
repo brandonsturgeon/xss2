bfb4e3ed7c925cbb79835a629fdbb660830916b6
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-06 12:37:27 -0500

    Update this.js

--- this.js
+++ this.js
@@ -1,2 +1,7 @@
-var generatedSource = new XMLSerializer().serializeToString(document);
-console.log(generatedSource);
+var grill = ""
+while (1===1) {
+  grill = grill + "meat";
+  console.log(grill);
+  console.log("It's getting spicy")
+}
+// spicy
