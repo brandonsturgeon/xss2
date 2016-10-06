bc94f08fb14c46a72f64066ed263b36f5e71c46a
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-05 19:24:28 -0500

    Update this.js

--- this.js
+++ this.js
@@ -1,3 +1,1 @@
-console.log('loaded');
-var el = document.querySelector("#main > table:nth-child(2) > tbody > tr > td:nth-child(1) > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(1) > img");
-console.log(el);
+alert('test');
