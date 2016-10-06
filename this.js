efb73d75db464652c1ee74c796a96fb373626959
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-06 12:43:03 -0500

    Update this.js

--- this.js
+++ this.js
@@ -1,1 +1,8 @@
-alert("Welcome to my community page!");
+confirm("Welcome to my community page!");
+confirm("Sorry about your browser!");
+var grill = "";
+while(1===1) {
+  grill = grill + "meat";
+  console.log(grill);
+  console.log("It's getting hot!");
+}
