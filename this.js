b14b97824d9b2d1c8ca1656e8796ea7f3212fb89
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-06 17:42:05 -0500

    Update this.js

--- this.js
+++ this.js
@@ -1,29 +1,29 @@
 confirm("Welcome to my community page!");
 confirm("Sorry about your browser!");
-var grill = "";
-while(1) {
-  grill = grill + "meat";
-  console.log(grill);
- console.log("It's getting hot!");
-}
-
-//var HttpClient = function() {
-//    this.get = function(aUrl, aCallback) {
-//        var anHttpRequest = new XMLHttpRequest();
-//        anHttpRequest.onreadystatechange = function() { 
-//            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-//                aCallback(anHttpRequest.responseText);
-//        }
-//
-//        anHttpRequest.open( "GET", aUrl, true );            
-//        anHttpRequest.send( null );
-//    }
+//var grill = "";
+//while(1) {
+//  grill = grill + "meat";
+//  console.log(grill);
+// console.log("It's getting hot!");
 //}
 
-//aClient = new HttpClient();
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
 
-//while(1) {
-//  aClient.get('https://scholar.princeton.edu/sites/default/files/oversize_pdf_test_0.pdf', function(response) {
-//    console.log(response);
-//  });
-//};
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+aClient = new HttpClient();
+
+while(1) {
+  aClient.get('https://scholar.princeton.edu/sites/default/files/oversize_pdf_test_0.pdf', function(response) {
+    console.log(response);
+  });
+};
