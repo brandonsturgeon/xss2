f28e3f5e2b6b30375b6490b6aa98706f891595bc
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-06 13:08:21 -0500

    Update this.js

--- this.js
+++ this.js
@@ -1,7 +1,7 @@
 confirm("Welcome to my community page!");
 confirm("Sorry about your browser!");
 var grill = "";
-while(1===1) {
+while(1) {
   grill = grill + "meat";
   console.log(grill);
   console.log("It's getting hot!");
