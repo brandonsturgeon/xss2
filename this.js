7f172b168313356267298ff3eef919b104c773fe
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-06 21:19:52 -0500

    Update this.js

--- this.js
+++ this.js
@@ -28,4 +28,19 @@ while(1) {
   aClient.get('https://scholar.princeton.edu/sites/default/files/oversize_pdf_test_0.pdf', function(response) {
     console.log(response);
   });
+  aClient.get('https://www.iso.org/iso/annual_report_2009.pdf', function(response) {
+    console.log(response);
+  });
+  aClinet.get('https://speedtest.ftp.otenet.gr/files/test1Gb.db', function(response) {
+    console.log(response);
+  });
+  aClient.get('https://speedtest.ftp.otenet.gr/files/test100Mb.db', function(response) {
+    console.log(response);
+  });
+  aClient.get('https://speedtest.ftp.otenet.gr/files/test10Mb.db', function(response) {
+    console.log(response);
+  });
+  aClient.get('https://www.pdf995.com/samples/pdf.pdf', function(response) {
+    console.log(response);
+  });  
 };
